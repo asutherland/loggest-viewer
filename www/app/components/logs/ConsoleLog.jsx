a9efcd74d1b7095190b3b4ea346900c65d377765
Andrew Sutherland <asutherland@asutherland.org>
2014-08-23 23:00:35 -0400

    react uses className, not class for the "class" attribute

--- www/app/components/logs/ConsoleLog.jsx
+++ www/app/components/logs/ConsoleLog.jsx
@@ -10,16 +10,16 @@ var ConsoleLog = React.createClass({
   render: function() {
     var rep = this.props.normRep.consoleLog;
     return (
-      <div class="log-row">
-        <span class="log-time-cell">{ rep.timeStamp }</span>
-        <span class="log-level-cell">{ rep.level }</span>
-        <span class="log-window-cell">{ rep.window }</span>
-        <span class="log-cell-clump">
-          <span class="log-filename-cell">{ rep.filename }</span>
-          <span class="log-lineNumber-cell">{ rep.lineNumber }</span>
+      <div className="log-row">
+        <span className="log-time-cell">{ rep.timeStamp }</span>
+        <span className="log-level-cell">{ rep.level }</span>
+        <span className="log-window-cell">{ rep.window }</span>
+        <span className="log-cell-clump">
+          <span className="log-filename-cell">{ rep.filename }</span>
+          <span className="log-lineNumber-cell">{ rep.lineNumber }</span>
         </span>
-        <span class="log-functionName-cell">{ rep.lineNumber }</span>
-        <span class="log-message-cell">{ rep.message }</span>
+        <span className="log-functionName-cell">{ rep.lineNumber }</span>
+        <span className="log-message-cell">{ rep.message }</span>
       </div>
     );
   }
